0bd31399147ea510441d1168dc2919e17e8fc56e
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-11 21:36:00 -0300

    Melhorando código

--- src/main/java/org/lopeshenriquee/models/ConsultaCEP.java
+++ src/main/java/org/lopeshenriquee/models/ConsultaCEP.java
@@ -12,20 +12,19 @@ public class ConsultaCEP {
     public Endereco buscaEndereco(String cep){
         URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
 
-        HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = null;
         try {
-            response = HttpClient
+            HttpResponse<String> response = HttpClient
                     .newHttpClient()
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException |  InterruptedException e) {
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception e) {
             throw new RuntimeException("Não consegui obter a url a apartir deste CEP");
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
+
     }
 }
