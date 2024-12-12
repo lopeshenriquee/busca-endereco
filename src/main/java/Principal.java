b7c7273be24bcaf1c6f2b1bbcae53e5ed9185e6c
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-11 21:25:32 -0300

    testando um request, funcionadno

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,10 @@
+import org.lopeshenriquee.models.ConsultaCEP;
+import org.lopeshenriquee.models.Endereco;
+
 public class Principal {
     public static void main(String[] args) {
-
+        ConsultaCEP consultaCEP = new ConsultaCEP();
+        Endereco novoEndereco = new ConsultaCEP().buscaEndereco("01001000");
+        System.out.println(novoEndereco);
     }
 }
