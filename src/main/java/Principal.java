8117a039bad0a5dba13fdee4ff698b2a40503698
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-12-11 21:45:49 -0300

    Classe principal com scanner e excecoes

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,10 +1,27 @@
 import org.lopeshenriquee.models.ConsultaCEP;
 import org.lopeshenriquee.models.Endereco;
+import org.lopeshenriquee.models.GeradorJson;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         ConsultaCEP consultaCEP = new ConsultaCEP();
-        Endereco novoEndereco = new ConsultaCEP().buscaEndereco("01001000");
-        System.out.println(novoEndereco);
+
+        System.out.println("Digite um CEP: ");
+        var cep = scan.nextLine();
+
+        try {
+            Endereco novoEndereco = new ConsultaCEP().buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorJson geradorTxt = new GeradorJson();
+            geradorTxt.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando Aplicação");
+        }
+        scan.close();
     }
 }
